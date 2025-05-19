3a41f4af2604314b2f75e94b5f17b638e499eb02
Jenifer Urbano <jdurbanocordoba@gmail.com>
2025-05-18 21:24:30 -0500

    Update transaccion.py

--- transaccion.py
+++ transaccion.py
@@ -1,25 +1,20 @@
-from enum import Enum
+from tipo import Tipo
 
 class Transaccion:
+    def __init__(self, tipo: Tipo, fecha: str, cantidad: int):
+        self.__tipo = tipo
+        self.__fecha = fecha
+        self.__cantidad = cantidad
     
-    class Tipo(Enum):
-        VENTA = 1
-        ABASTECIMIENTO = 2
-    
-
-    def __init__(self, pTipo: Tipo, pCantidad: int):
-        self.__tipo = pTipo
-        self.__fecha = "Fecha actual" 
-        self.__cantidad = pCantidad
-    
-
-    def darTipo(self):
+    def darTipo(self) -> Tipo:
         return self.__tipo
-    
-
-    def darFecha(self):
+        
+    def darFecha(self) -> str:
         return self.__fecha
-    
-    
-    def darCantidad(self):
+        
+    def darCantidad(self) -> int:
         return self.__cantidad
+    
+    def toString(self) -> str:  #aparece en el diagrama mostrado en clase, devuelve una representación en cadena de la transacción.
+        tipo_str = "Venta" if self.__tipo == Tipo.VENTA else "Abastecimiento"
+        return f"{tipo_str} - Fecha: {self.__fecha} - Cantidad: {self.__cantidad}"
